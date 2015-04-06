39fc3c8c2814e40d41c0d98784bc7e3baf8b137b
Nico <nico-2006@web.de>
2015-04-06 16:31:29 +0200

    Updated Hello World example

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2014 NicoHood
+ Copyright (c) 2014-2015 NicoHood
  See the readme for credit to other people.
 
  Hello World Keyboard Beginner example
@@ -16,16 +16,23 @@ void setup() {
   pinMode(pinLed, OUTPUT);
   pinMode(pinButton, INPUT_PULLUP);
 
+  // Serial setup
+  Serial.begin(115200);
+
   // Sends a clean report to the host. This is important on any Arduino type.
   Keyboard.begin();
 }
 
 void loop() {
+  // check if button is pressed
   if (digitalRead(pinButton) == LOW) {
     digitalWrite(pinLed, HIGH);
 
     // Same use as the official library, pretty much self explaining
     Keyboard.println("Hello World");
+
+    // check if Serial port is still working
+    Serial.println("Working Serial");
 
     // simple debounce
     delay(500);
