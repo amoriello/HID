42e5a14a6396dc47f41f562b3a527c15bcda3086
Nico <nico-2006@web.de>
2015-04-06 18:34:32 +0200

    Case Sensitive fix

--- avr/cores/hid/USB-Core/MidiUSB.cpp
+++ avr/cores/hid/USB-Core/MidiUSB.cpp
@@ -21,7 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include "MIDIUSB.h"
+#include "MidiUSB.h"
 #include <avr/wdt.h>
 
 #if defined(USBCON)
